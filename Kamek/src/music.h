534272c976252bc7431787f05025427c8ee80561
wakanameko <63937252+wakanameko@users.noreply.github.com>
2023-08-27 13:26:10 +0900

    Delete Kamek directory

--- Kamek/src/music.h
+++
@@ -1,4 +0,0 @@
-#ifndef MUSIC_H
-#define MUSIC_H 
-u8 hijackMusicWithSongName(const char *songName, int themeID, bool hasFast, int channelCount, int trackCount, int *wantRealStreamID);
-#endif /* MUSIC_H */
